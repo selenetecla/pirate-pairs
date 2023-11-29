da66dfd5b8f872457f6bc1bb989184fe5d4313c9
selenetecla <149593026+selenetecla@users.noreply.github.com>
2023-11-29 14:41:04 +0000

    deck

--- src/main/Deck.java
+++ src/main/Deck.java
@@ -2,19 +2,23 @@ import java.util.Arrays;
 import java.util.Random;
 public class Deck {
     final int numOfCards = 55;
+    private int[] cards = new int [numOfCards];
     public Deck() {
-        int[] deck = new int [numOfCards];
         int cardNumber = 1;
-        for (int i = 0; i < deck.length; i++) {
-            deck[i] = cardNumber;
+        for (int i = 0; i < cards.length; i++) {
+            cards[i] = cardNumber;
             cardNumber = (cardNumber % 10) + 1;
         }
     }
-        public void shuffleDeck() {
-        for (int i = deck.length - 1; i > 0; i--) {
+    public String getArray() { 
+        return ;
+    }
+    public void shuffleDeck() {
+        for (int i = cards.length - 1; i > 0; i--) {
             int index = (i + 1);
-            int temp = deck[i];
-            deck[i] = deck[index];
-            deck[index] = temp;
+            int temp = cards[i];
+            cards[i] = cards[index];
+            cards[index] = temp;
         }
+    }
 }
--- src/main/PiratePairs.java
+++ src/main/PiratePairs.java
@@ -1,7 +1,8 @@
 import java.util.Arrays;
-
 public class PiratePairs {
     public static void main(String[] args) {
-        System.out.println("");
+        Deck deck = new Deck();
+        deck.shuffleDeck();
+        System.out.println(Arrays.toString(deck));
     }
 }
